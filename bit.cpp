1cdc4f6bc0c0d234784011e6a38c17f69c2f091e
xehoth <824547322@qq.com>
2017-11-14 21:36:19 +0800

    expand tab

--- bit.cpp
+++ bit.cpp
@@ -3,18 +3,18 @@ using namespace std;
 #include "clz.cpp"
 
 int main() {
-	clz_init();
-	clock_t t = clock();
-	unsigned ans = 0;
-	for (long long i = 1; i < 100000000; ++i) {
-		ans += __builtin_clzll(i);
-	}
-	cout << clock() - t << ' ' << ans << endl;
-	t = clock();
-	ans = 0;
-	for (long long i = 1; i < 100000000; ++i) {
-		ans += clzll(i);
-	}
-	cout << clock() - t << ' ' << ans << endl;
-	return ans;
+    clz_init();
+    clock_t t = clock();
+    unsigned ans = 0;
+    for (long long i = 1; i < 100000000; ++i) {
+        ans += __builtin_clzll(i);
+    }
+    cout << clock() - t << ' ' << ans << endl;
+    t = clock();
+    ans = 0;
+    for (long long i = 1; i < 100000000; ++i) {
+        ans += clzll(i);
+    }
+    cout << clock() - t << ' ' << ans << endl;
+    return ans;
 }
